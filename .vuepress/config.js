9de13bf4eab7a2c2f08542178d8817192d704ac1
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-06-05 14:35:16 +0200

    Add VuePress

---
+++ .vuepress/config.js
@@ -0,0 +1,34 @@
+sidebar = [
+  'animation',
+  'accompagnement',
+  'communication',
+  {
+    title: 'Bonnes pratiques',
+    collapsable: true,
+    children: [
+      ['opensource', "Open source"],
+      'opensource-licences',
+      'opendata',
+      ['accueil-eig', "Accueillir des EIG"]
+    ]
+  },
+]
+
+module.exports = {
+  theme: 'gouv-fr',
+  title: 'Documentation du programme EIG',
+  description: "La documentation interne du programme Entrepreneurs d'Intérêt Général.",
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.png' }],
+    ['meta', { name: 'theme-color', content: '#0053b3' }]
+  ],
+  themeConfig: {
+    sidebar: sidebar,
+    sidebarDepth: 1,
+    docsDir: ".",
+    repo: 'entrepreneur-interet-general/eig-link',
+    repoLabel: 'Dépôt Git',
+    editLinks: true,
+    editLinkText: 'Modifier cette page'
+  }
+}
